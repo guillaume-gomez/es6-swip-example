b3955e7145abad06ad2d04b1898c8543c08fec3e
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-21 00:05:41 +0200

    wip draw the same stuff as the demo

--- client/phaser/src/constants.js
+++ client/phaser/src/constants.js
@@ -1,19 +1,5 @@
 export const CellWidth = 50;
 export const ElapsedTime = 500;
 export const SizeTerrain = 51;
-export const AntsColor = [
-	"brown",
-	"red",
-	"yellow",
-	"grey"
-];
-
-export function convertToNumberColor(color) {
-	if(!AntsColor.includes(color)) {
-		return 0;
-	}
-	return AntsColor.indexOf(color);
-}
-
 export const WidthCanvas = 800;
-export const HeighCanvas = 600;
+export const HeighCanvas = 400;
--- client/phaser/src/states/GameState.js
+++ client/phaser/src/states/GameState.js
@@ -1,7 +1,3 @@
-import Grid from '../objects/Grid';
-import Ant from '../objects/Ant';
-import History from '../objects/History';
-
 import { CellWidth, ElapsedTime, SizeTerrain, WidthCanvas, HeighCanvas } from '../constants';
 
 const CameraVelocity = 10;
@@ -11,67 +7,13 @@ class GameState extends Phaser.State {
 
 	create() {
 		this.game.world.setBounds(0, 0, Bounds, Bounds);
-		this.gridLayout = new Grid(this.game, Bounds/ CellWidth, Bounds/CellWidth, CellWidth);
 		this.cursors = this.game.input.keyboard.createCursorKeys();
-
-		this.ant = new Ant(this.game, 0, 0, "red");
-		this.game.add.existing(this.ant);
-
-		this.textStep = this.game.add.text(this.game.world.width * 0.01, this.game.world.height * 0.01, "Steps: ", { font: "18px Arial", fill: "#0000FF", align: "center" });
-		this.textStep.fixedToCamera = true;
-		this.setAntCenter();
-
-		this.steps = 0;
-		this.timer = this.game.time.create(false);
-		this.timer.add(window.ElapsedTime || ElapsedTime, this.updatePosition, this);
-		this.timer.start();
-
-        this.game.time.advancedTiming = true;
-
-	    this.replay = new History();
-		this.replay.start();
-    	this.replay.recordStep(this.steps, this.ant, this.gridLayout.getCellsArray());
-
     this.initCamera();
 	}
 
   initCamera() {
     this.game.camera.x = this.game.world.width / 2 - WidthCanvas / 2;
     this.game.camera.y = this.game.world.height / 2 - HeighCanvas / 2;
-  }
-
-	setAntCenter() {
-		const indexX = Math.trunc( (Bounds / CellWidth) / 2 );
-		const indexY = Math.trunc( (Bounds / CellWidth) / 2 );
-    this.setAntOnCell(indexX, indexY);
-	}
-
-	setAntOnCell(indexX, indexY) {
-		const cell = this.gridLayout.getCell(indexX, indexY);
-		this.ant.x = cell.realPosition.x + CellWidth / 2 - this.ant.width / 2;
-		this.ant.y = cell.realPosition.y + CellWidth / 2 - this.ant.height / 2;
-	}
-
-	preload() {
-    this.game.stage.disableVisibilityChange = true;
-    this.game.load.spritesheet('ant', 'res/ants.png', 32, 32);
-  }
-
-	updatePosition() {
-    let cell = this.gridLayout.getCellAtPosition( this.ant.x, this.ant.y );
-		this.ant.updateAnt(cell);
-		cell.toggle();
-		this.steps += 1;
-		this.textStep.text = "Steps :" + this.steps;
-		this.timer.add(window.ElapsedTime || ElapsedTime, this.updatePosition, this);
-		this.replay.recordStep(this.steps, this.ant, this.gridLayout.getCellsArray());
-	}
-
-  setSimulationTo(step) {
-    const simulationData = this.replay.getTo(step);
-    this.gridLayout.setStates(simulationData.grid);
-    this.ant.goTo(simulationData.x, simulationData.y);
-    this.steps = step;
   }
 
 	update() {
@@ -94,6 +36,35 @@ class GameState extends Phaser.State {
 	    	this.game.camera.x += CameraVelocity;
 	    }
 	}
+
+	drawBall (ball) {
+		var bmd = game.add.bitmapData(128,128);
+    //ctx.save();
+
+    bmd.ctx.fillStyle = '#fff';
+    bmd.ctx.shadowBlur = 10;
+    bmdd.ctx.shadowColor = 'rgba(0, 0, 0, 0.2)';
+
+    bmd.ctx.beginPath();
+    bmd.ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+    bmd.ctx.fill();
+
+    var sprite = game.add.sprite(200, 200, bmd);
+  }
+
+  
+  drawHole (hole) {
+    var bmd = game.add.bitmapData(128,128);
+
+    bmd.ctx.fillStyle = 'black';
+    bmd.ctx.strokeStyle = '#4b7f1f';
+    bmd.ctx.lineWidth = 2;
+
+    bmd.ctx.beginPath();
+    bmd.ctx.arc(hole.x, hole.y, hole.radius, 0, 2 * Math.PI);
+    bmd.ctx.fill();
+    bmd.ctx.stroke();
+  }
 
 	render() {
 		//this.game.debug.spriteBounds(this.ant);
