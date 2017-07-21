9209bc68e64a31242b31d2e4cf2b431c7acde304
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-22 00:37:18 +0200

    change constant

--- client/phaser/src/constants.js
+++ client/phaser/src/constants.js
@@ -2,4 +2,4 @@ export const CellWidth = 50;
 export const ElapsedTime = 500;
 export const SizeTerrain = 51;
 export const WidthCanvas = 800;
-export const HeighCanvas = 400;
+export const HeighCanvas = 500;
